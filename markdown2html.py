ee47d3d1d1a76d302d77f0e9885bdf79900fb2c5
AbduHany <abdu.hany@gmail.com>
2024-08-11 16:18:07 +0300

    Add unordered listing parsing - markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -10,21 +10,39 @@ def convert(markdownFile, htmlFile):
     try:
         with open(markdownFile, 'r') as r:
             with open(htmlFile, 'w') as w:
-                for line in r:
+                text = r.read()
+                textLines = text.splitlines()
+                i = 0
+                while i < len(textLines):
+                    line = textLines[i]
+                    if line == '':
+                        i += 1
+                        continue
                     if line.startswith('######'):
-                        w.write('<h6>{}</h6>\n'.format(line[7:-1]))
+                        w.write('<h6>{}</h6>\n'.format(line[7:]))
                     elif line.startswith('#####'):
-                        w.write('<h5>{}</h5>\n'.format(line[6:-1]))
+                        w.write('<h5>{}</h5>\n'.format(line[6:]))
                     elif line.startswith('####'):
-                        w.write('<h4>{}</h4>\n'.format(line[5:-1]))
+                        w.write('<h4>{}</h4>\n'.format(line[5:]))
                     elif line.startswith('###'):
-                        w.write('<h3>{}</h3>\n'.format(line[4:-1]))
+                        w.write('<h3>{}</h3>\n'.format(line[4:]))
                     elif line.startswith('##'):
-                        w.write('<h2>{}</h2>\n'.format(line[3:-1]))
+                        w.write('<h2>{}</h2>\n'.format(line[3:]))
                     elif line.startswith('#'):
-                        w.write('<h1>{}</h1>\n'.format(line[2:-1]))
+                        w.write('<h1>{}</h1>\n'.format(line[2:]))
+                    elif line.startswith('-'):
+                        listStr = ""
+                        for j in range(i, len(textLines)):
+                            if textLines[j].startswith('-'):
+                                listStr += "<li>{}</li>\n".format(
+                                    textLines[j][2:])
+                            else:
+                                break
+                        w.write("<ul>\n{}</ul>\n".format(listStr))
+                        i = j
                     else:
                         w.write(line)
+                    i += 1
 
     except Exception:
         print("Missing {}".format(markdownFile), file=sys.stderr)
