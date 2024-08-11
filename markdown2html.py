32dd60a3a3a2890245ed01c8cc69e8edf01d13b7
AbduHany <abdu.hany@gmail.com>
2024-08-11 15:55:10 +0300

    Add headings rendering to markdowntohtml.py

--- markdown2html.py
+++ markdown2html.py
@@ -8,8 +8,24 @@ def convert(markdownFile, htmlFile):
     """ Converts markdown to HTML.
     """
     try:
-        with open(markdownFile, 'r') as f:
-            markdown = f.read()
+        with open(markdownFile, 'r') as r:
+            with open(htmlFile, 'w') as w:
+                for line in r:
+                    if line.startswith('######'):
+                        w.write('<h6>{}</h6>\n'.format(line[7:-1]))
+                    elif line.startswith('#####'):
+                        w.write('<h5>{}</h5>\n'.format(line[6:-1]))
+                    elif line.startswith('####'):
+                        w.write('<h4>{}</h4>\n'.format(line[5:-1]))
+                    elif line.startswith('###'):
+                        w.write('<h3>{}</h3>\n'.format(line[4:-1]))
+                    elif line.startswith('##'):
+                        w.write('<h2>{}</h2>\n'.format(line[3:-1]))
+                    elif line.startswith('#'):
+                        w.write('<h1>{}</h1>\n'.format(line[2:-1]))
+                    else:
+                        w.write(line)
+
     except Exception:
         print("Missing {}".format(markdownFile), file=sys.stderr)
         sys.exit(1)
